c4e9ba7ccd1ab91c54e7196234a03d51fba1cc35
geovannyguaman <geovannyjguaman@coopdaquilema.com>
2025-11-25 19:39:36 -0500

    Sprint__002 feat: roles_repository_impl.dart

---
+++ lib/modules/roles/data/repository/roles_repository_impl.dart
@@ -0,0 +1,90 @@
+import 'package:dartz/dartz.dart';
+import 'package:resturant_funny/core/services/conection_service.dart';
+import 'package:resturant_funny/core/utils/either.dart';
+import 'package:resturant_funny/modules/authentication/domain/entity/rol_entity.dart';
+import 'package:resturant_funny/modules/roles/data/datasource/roles_remote_datasource.dart';
+import 'package:resturant_funny/modules/roles/domain/roles_repository.dart';
+
+class RolesRepositoryImpl implements RolesRepository {
+  final RolesRemoteDataSource remote;
+  final ConnectivityService _connectivity = ConnectivityService();
+
+  RolesRepositoryImpl(this.remote) {
+    _connectivity.initialize();
+  }
+
+  @override
+  Future<Either<Failure, List<RolEntity>>> cargarRoles() async {
+    final isConnected = await _connectivity.checkConnection();
+    if (!isConnected) {
+      return const Left(NetworkFailure('No hay conexión a internet'));
+    }
+    try {
+      final roles = await remote.cargarRoles();
+      return Right(roles);
+    } catch (_) {
+      return const Left(
+          ServerFailure('Ha ocurrido un error, inténtalo más tarde'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, RolEntity>> createRol({
+    required String estado,
+    required String nombre,
+    required String observacion,
+  }) async {
+    final isConnected = await _connectivity.checkConnection();
+    if (!isConnected) {
+      return const Left(NetworkFailure('No hay conexión a internet'));
+    }
+    try {
+      final roles = await remote.createRol(
+        nombre: nombre,
+        observacion: observacion,
+        estado: estado,
+      );
+      return Right(roles);
+    } catch (_) {
+      return const Left(
+        ServerFailure('Ha ocurrido un error, inténtalo más tarde'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> deleteRol(RolEntity rol) async {
+    final isConnected = await _connectivity.checkConnection();
+    if (!isConnected) {
+      return const Left(NetworkFailure('No hay conexión a internet'));
+    }
+    try {
+      final isDelete = await remote.deleteRol(rol);
+      return Right(isDelete);
+    } catch (_) {
+      return const Left(
+        ServerFailure('Ha ocurrido un error, inténtalo más tarde'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, RolEntity>> updateRol(RolEntity rol,
+      {required String nombre,
+      required String observacion,
+      required String estado}) async {
+    final isConnected = await _connectivity.checkConnection();
+    if (!isConnected) {
+      return const Left(NetworkFailure('No hay conexión a internet'));
+    }
+    try {
+      final rolUpdate = await remote.updateRol(rol,
+          nombre: nombre, observacion: observacion, estado: estado);
+      return Right(rolUpdate);
+    } catch (_) {
+      return const Left(
+        ServerFailure('Ha ocurrido un error, inténtalo más tarde'),
+      );
+    }
+  }
+}
