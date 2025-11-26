ae77485a1df9a9e5c9c4f597e795ea3fada7a47d
geovannyguaman <geovannyjguaman@coopdaquilema.com>
2025-11-25 19:39:54 -0500

    Sprint__002 feat: roles_repository.dart

---
+++ lib/modules/roles/domain/roles_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:resturant_funny/core/utils/either.dart';
+import 'package:resturant_funny/modules/authentication/domain/entity/rol_entity.dart';
+
+abstract class RolesRepository {
+  Future<Either<Failure, List<RolEntity>>> cargarRoles();
+
+  Future<Either<Failure, RolEntity>> createRol(
+      {required String nombre,
+      required String observacion,
+      required String estado});
+
+  Future<Either<Failure, RolEntity>> updateRol(RolEntity rol,
+      {required String nombre,
+      required String observacion,
+      required String estado});
+
+  Future<Either<Failure, bool>> deleteRol(RolEntity rol);
+}
